dd1c36233b3c4632fddb86ab5043f3b3a62591ae
deepak00944 <deepak.s.ashta@gmail.com>
2025-04-30 16:39:04 +0530

    Scrolling issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import ScrollToTop from "./components/ScrollToTop";
 import ArticleDetailPage from "./pages/ArticleDetailPage"
 import CategoriesPage from "./pages/CategoriesPage"
 import AboutPage from "./pages/AboutPage"
@@ -12,6 +13,7 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <ScrollToTop/>
         <Navbar />
         <main className="main-content">
           <Routes>
